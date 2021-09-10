ab15f807fd80340b9c91cf6399703596c7243bf9
manamu-kawaguchi <89883580+manamu-kawaguchi@users.noreply.github.com>
2021-09-10 10:56:41 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'だいやもんどすかいつりー',
         },
         stage2: {
           q1: 'いいい',
